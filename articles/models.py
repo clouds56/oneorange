73e6d6638d773921ccc659ec87593c5be63b196b
Clouds <Clouds.Flowing@gmail.com>
2014-05-25 17:20:57 +0800

    models add __str__ method

--- articles/models.py
+++ articles/models.py
@@ -4,9 +4,13 @@ from django.db import models
 class Author(models.Model):
   name = models.CharField(max_length=100)
   reg_date = models.DateTimeField('date registered')
+  def __str__(self):
+    return self.name
 
 class Article(models.Model):
   title = models.CharField(max_length=200)
   content = models.CharField(max_length=20000)
   pub_date = models.DateTimeField('date published')
   author = models.ForeignKey(Author)
+  def __str__(self):
+    return self.title+"\n\n"+self.content
