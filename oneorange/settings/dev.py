dee56e39de2039d3cecc5339af9fc8925b39eaf1
Clouds <Clouds.Flowing@gmail.com>
2015-02-20 16:59:28 +0800

    restruct the project use django-html5-boilerplate
    see also https://github.com/mike360/django-html5-boilerplate
    setting -> dev

---
+++ oneorange/settings/dev.py
@@ -0,0 +1,51 @@
+# Django project environment-specific settings
+
+from oneorange.settings.base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+SITE_ID = 1
+
+#TODO: replace localhost with the domain name of the site
+DEFAULT_FROM_EMAIL = 'messenger@localhost'
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),   # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                       # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                       # Set to empty string for default.
+    }
+}
+
+
+TIME_ZONE = 'Asia/Shanghai'
+
+INTERNAL_IPS = ('127.0.0.1', )
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Overwrite default ROOT_URLCONF to include static file serving by Django.
+# In production, this should be handled separately by your webserver or CDN.
+ROOT_URLCONF = 'oneorange.urls.dev'
+
+BOOTSTRAP3 = {
+    'include_jquery': False,
+    'jquery_url': '/static/js/vendor/jquery-2.1.3.js',
+    'base_url': '/static/',
+    'css_url': '/static/css/bootstrap.css',
+    'theme_url': '/static/css/bootstrap-theme.css',
+    'javascript_url': '/static/js/vendor/bootstrap-3.3.2.js',
+}
--- oneorange/urls/base.py
+++ oneorange/urls/base.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+    # Home Page -- Replace as you prefer
+    #url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^$', 'oneorange.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^articles/', include('articles.urls')),
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
