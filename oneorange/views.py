7114697739f889195400f48b1b3dfc8382a006a5
Clouds <Clouds.Flowing@gmail.com>
2015-02-20 18:35:45 +0800

    fix typo in detail.html

--- oneorange/views.py
+++ oneorange/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("""<h1>Welcome to one-orange</h1><a href="articles/">articles</a> <a href="admin/">admin</a>""")
+    return HttpResponse("""<h1>Welcome to Orangez</h1><a href="articles/">articles</a> <a href="admin/">admin</a>""")
