163b407d72e0c816ea5ddb6c3012f8649e97589f
Clouds <Clouds.Flowing@gmail.com>
2015-02-20 19:09:40 +0800

    fix warnning test runner

--- oneorange/settings/base.py
+++ oneorange/settings/base.py
@@ -130,6 +130,8 @@ INSTALLED_APPS = (
     'articles',
 )
 
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
