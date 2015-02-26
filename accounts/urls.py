0698d03d6c8f41c02b0cd1e7df69ee443e438d84
Clouds <Clouds.Flowing@gmail.com>
2015-02-26 23:42:55 +0800

    step2 create author

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^login/?$', views.login, name = 'login'),
     url(r'^logout/?$', views.logout, name = 'logout'),
     url(r'^signup/?$', views.signup, name = 'signup'),
+    url(r'^step2(/(?P<author>[^/]+))?/?$', views.step2, name = 'step2'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -36,10 +36,27 @@ def signup(request):
         return render(request, "signup.html", {'error_msg': "No password"})
     if not 'email' in request.POST:
         return render(request, "signup.html", {'error_msg': "No email"})
-    if User.objects.filter(username=request.POST['username']).exists():
-        return render(request, "signup.html", {'error_msg': "Username exists"})
-    if User.objects.filter(email=request.POST['email']).exists():
-        return render(request, "signup.html", {'error_msg': "Email exists"})
-    user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    if User.objects.filter(username=username).exists():
+        return render(request, "signup.html", {'error_msg': "Username exists", 'last': {'username': username, 'email':email}})
+    if User.objects.filter(email=email).exists():
+        return render(request, "signup.html", {'error_msg': "Email exists", 'last': {'username': username, 'email':email}})
+    user = User.objects.create_user(username, email, password)
     user.save()
-    return render(request, "success.html")
+    return redirect("/accounts/step2/"+username)
+
+def step2(request, author):
+    if not 'csrfmiddlewaretoken' in request.POST:
+        return render(request, "step2.html", {'author': author})
+    if request.user.is_authenticated():
+        username = request.user.username
+    if not request.POST['author']:
+        return render(request, "step.html", {'error_msg': "No author name", 'author': author})
+    author = request.POST['author']
+    if Author.objects.filter(name=author).exists():
+        return render(request, "step.html", {'error_msg': "Author name exists", 'author': author})
+    anauthor = Author.objects.create(name=author, user=request.user)
+    anauthor.save()
+    return redirect("/accounts/success")
