153c7efb79d0c4c2cae7be53c0df47b42389291d
Clouds <Clouds.Flowing@gmail.com>
2015-02-28 21:33:08 +0800

    fix login redirect to referer

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
+from common.util import get_referer_view, get_relative_url
 from articles.models import Author, Article, Anthology
 # Create your views here.
 
@@ -15,24 +16,39 @@ def index(request):
     return HttpResponse(template.render(context))
 
 def login(request):
-    referer = "/articles"
-    if request.META.get('HTTP_REFERER'):
-        referer = request.META.get('HTTP_REFERER')
-    if not 'csrfmiddlewaretoken' in request.POST:
-        return render(request, "login.html")
+    referer = get_referer_view(request, "/articles")
+    if referer.find("/accounts/login") == 0:
+        referer = "/articles"
+
+    if request.method == 'GET':
+        if not 'next' in request.GET or not request.GET['next']:
+            return redirect("/accounts/login?next="+referer)
+        return render(request, "login.html", {'next': request.GET['next']})
+
+    next = ""
+    if 'next' in request.POST and request.POST['next']:
+        next = get_relative_url(request.POST['next'], request.META.get('SERVER_NAME'), referer)
+    elif 'next' in request.GET and request.GET['next']:
+        next = get_relative_url(request.GET['next'], request.META.get('SERVER_NAME'), referer)
+    else:
+        next = referer
+    if next.find("/accounts/login") == 0:
+        next = referer;llllkkk
     if request.user.is_authenticated():
-        return redirect(request.META.get('HTTP_REFERER'))
+        return redirect(next)
     if not 'username' in request.POST:
-        return render(request, "login.html", {'error_msg': "No username"})
+        return render(request, "login.html", {'error_msg': "No username", 'next': next})
     if not 'password' in request.POST:
-        return render(request, "login.html", {'error_msg': "No password"})
+        return render(request, "login.html", {'error_msg': "No password", 'next': next})
     username = request.POST['username']
+    if User.objects.filter(username=username).exists():
+        return render(request, "login.html", {'error_msg': "User not exist", 'next': next})
     password = request.POST['password']
     user = auth.authenticate(username=username, password=password)
     if not user:
-        return render(request, "login.html", {'error_msg': "User not exist"})
+        return render(request, "login.html", {'error_msg': "User or password wrong", 'next': next})
     auth.login(request, user)
-    return redirect(request.META.get('HTTP_REFERER'))
+    return redirect(next)
 
 @login_required
 def logout(request):
---
+++ common/util.py
@@ -0,0 +1,34 @@
+import re
+
+def get_referer_view(request, default=None):
+    ''' 
+    Return the referer view of the current request
+
+    Example:
+
+        def some_view(request):
+            ...
+            referer_view = get_referer_view(request)
+            return HttpResponseRedirect(referer_view, '/accounts/login/')
+    '''
+
+    # if the user typed the url directly in the browser's address bar
+    referer = request.META.get('HTTP_REFERER')
+    if not referer:
+        return default
+
+    # remove the protocol and split the url at the slashes
+    return get_relative_url(referer, request.META.get('SERVER_NAME'), default)
+
+def get_relative_url(url, server=None, default=None):
+    if not url:
+        return default
+
+    # remove the protocol and split the url at the slashes
+    urltree = re.sub('^https?:\/\/', '', url).split('/')
+    if server and urltree[0] != server:
+        return default
+
+    # add the slash at the relative path's view and finished
+    url = u'/' + u'/'.join(urltree[1:])
+    return url
