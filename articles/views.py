1fe4d45711a7b0d7af201755a044a88bf29e7b6b
Clouds <Clouds.Flowing@gmail.com>
2015-04-05 10:22:43 +0800

    add create anthology

--- articles/views.py
+++ articles/views.py
@@ -11,15 +11,34 @@ def index(request):
     authors = Author.objects.all()
     return render(request, "base.html", {'authors': authors})
 
-def author(request, author):
-    author = get_object_or_404(Author, name=author)
-    author_anthologies = author.anthologies.all()
-    return render(request, "author.html", {'author': author, 'author_anthologies': author_anthologies})
+def author(request, author_name):
+    args = {}
+    args['author'] = get_object_or_404(Author, name=author_name)
+    if request.method == "GET":
+        args['author_anthologies'] = args['author'].anthologies.all()
+        return render(request, "author.html", args)
+    elif request.method == "POST":
+        anthology_name = ""
+        created = None
+        if "name" in request.POST:
+            anthology_name = request.POST["name"]
+        if anthology_name:
+            anthology, created = Anthology.objects.get_or_create(name=anthology_name, author=args['author'])
+        else:
+            args['author_anthologies'] = args['author'].anthologies.all()
+            args['msg'] = "error name"
+            render(request, "author.html", args)
+        if created:
+            args['msg'] = anthology_name + " created"
+        else:
+            args['msg'] = anthology_name + " already exist"
+        args['author_anthologies'] = args['author'].anthologies.all()
+        return render(request, "author.html", args)
 
-def anthology(request, author, anthology):
-    anthology = get_object_or_404(Anthology, author__name=author, name=anthology)
+def anthology(request, author_name, anthology_name):
+    anthology = get_object_or_404(Anthology, author__name=author_name, name=anthology_name)
     return render(request, "anthology.html", {'anthology': anthology})
 
-def article(request, author, anthology, article):
-    article = get_object_or_404(Article, id=article)
+def article(request, author_name, anthology_name, article_id):
+    article = get_object_or_404(Article, id=article_id)
     return render(request, "article.html", {'article': article})
